e0c23ba99bdc364b7c13f0e275ca2424fd6e19c2
carlosdiazz <c.diazadriann@gmail.com>
2022-10-25 11:31:22 -0400

    df

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost:4300/api/v1"
+export const BASE_API_URL = "https://backendmicroservicios-production.up.railway.app/api/v1"
 //export const BASE_API_URL = "http://192.168.0.181:4500/api/v1"
 
 export const TOKEN = "token";
